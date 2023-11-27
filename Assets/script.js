7eb1aa1b5274e46629c3366243a27b1e7aed090a
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2023-11-27 00:21:12 -0500

    got the fetch to work

--- Assets/script.js
+++ Assets/script.js
@@ -1,68 +1,77 @@
-let searchTerm = document.querySelector('#search-input');
+
 let formEl = document.getElementById('#form-card');
 let apiKey = "2ac4a3b4e64c6c4912def98a2d6f546d";
 
 
 let city;
 
-let cityCodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchTerm + '&appid=' + apiKey;
+
 
 
 
 document.getElementById("form-card").addEventListener("submit", getCityApi);
 
-function search() {
 
-    let searchResults = [];
 
-    // Display the search results in the main content
-    var resultsContainer = document.getElementById('searchResults');
-    resultsContainer.innerHTML = ("<h3>5 Day Forecast for </h3>" + city);
 
-    //  search result to the container
-    for (let i = 0; i < searchResults.length; i++) {
-        var resultItem = document.createElement('p');
-        resultItem.textContent = searchResults[i];
-        resultsContainer.appendChild(resultItem);
-    }
-}
 
-//fetch current weather
-function getWeatherApi() {
-
-    fetch(weatherUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        })
-}
-
-//fetch city information to get lon and lat
+//fetch city information to get lon and lat and then fetch current weather for the location using them
 function getCityApi() {
 
+    let searchTerm = document.querySelector('#search-input').value;
+    let cityCodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchTerm + '&appid=' + apiKey;
+    console.log(searchTerm);
+
+
     event.preventDefault();
+    //fetch city geolocation data and extract lon and lat
     fetch(cityCodeUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            let cityLat = data.lat;
-            let cityLon = data.lon;
+            let cityLat = data[0].lat;
+            let cityLon = data[0].lon;
             console.log(cityLat);
             console.log(cityLon);
+
+            let weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&units=metric&appid=' + apiKey;
+            //fetch current weather using lon and lat from previous fetch
+            fetch(weatherUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (current) {
+                    console.log(current)
+                    let currentTime = current.list[0].dt_txt;
+                    let weatherIconCode = current.list[0].weather[0].icon;
+                    let currentTemp = current.list[0].main.temp;
+                    let currentHumid = current.list[0].main.humidity;
+                    let currentWind = current.list[0].wind.speed;
+
+                    display();
+
+                    function display() {
+
+                        let searchTerm = document.querySelector('#search-input').value;
+                        // Display the search results in the main content
+                        let resultsContainer = document.getElementById('searchResults');
+                        resultsContainer.innerHTML = ("<h3>5 Day Forecast for </h3>" +
+                            searchTerm);
+
+                        let weatherResults = document.createElement('div');
+
+                        resultsContainer.append(weatherResults);
+
+                        weatherResults.innerHTML = ('<h5>Today</h5>' + currentTime + weatherIconCode + 'Temp: ' + currentTemp + 'c' + 'Humidity: ' + currentHumid + 'Wind Speed: ' + currentWind + 'kph');
+
+                    }
+
+                })
+
+
         });
 
-        let weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + apiKey;
-
-    fetch(weatherUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (current) {
-            console.log(current)
-        })
 
 }
 
