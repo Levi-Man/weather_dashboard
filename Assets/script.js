0bc458374ebd88372f944d30669cd0aa5d26d3ef
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2023-11-27 21:28:47 -0500

    Trying to add 5-day forecast but getting an error on js:89

--- Assets/script.js
+++ Assets/script.js
@@ -17,122 +17,162 @@ document.getElementById("form-card").addEventListener("submit", getCityApi);
 
 //fetch city information to get lon and lat and then fetch current weather for the location using them
 function getCityApi() {
-event.preventDefault();
+    event.preventDefault();
     // let searchTerm = document.querySelector('#search-input').value;
     let cityCodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchTerm.value + '&appid=' + apiKey;
     console.log(searchTerm.value);
 
     saveSearch();
 
-event.preventDefault();
-//fetch city geolocation data and extract lon and lat
-fetch(cityCodeUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        let cityLat = data[0].lat;
-        let cityLon = data[0].lon;
-        console.log(cityLat);
-        console.log(cityLon);
+    event.preventDefault();
+    //fetch city geolocation data and extract lon and lat
+    fetch(cityCodeUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let cityLat = data[0].lat;
+            let cityLon = data[0].lon;
+            console.log(cityLat);
+            console.log(cityLon);
 
-        let weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&units=metric&appid=' + apiKey;
-        //fetch current weather using lon and lat from previous fetch
-        fetch(weatherUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (current) {
-                console.log(current)
-                let currentTime = current.list[0].dt_txt;
-                let weatherIconCode = current.list[0].weather[0].icon;
-                let currentTemp = current.list[0].main.temp;
-                let currentHumid = current.list[0].main.humidity;
-                let currentWind = current.list[0].wind.speed;
+            let weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&units=metric&appid=' + apiKey;
+            //fetch current weather using lon and lat from previous fetch
+            fetch(weatherUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (current) {
+                    console.log(current)
+                    let currentTime = current.list[0].dt_txt;
+                    let weatherIconCode = current.list[0].weather[0].icon;
+                    let currentTemp = current.list[0].main.temp;
+                    let currentHumid = current.list[0].main.humidity;
+                    let currentWind = current.list[0].wind.speed;
 
-                display();
+                    display();
 
-                function display() {
+                    function display() {
 
-                    let searchTerm = document.querySelector('#search-input').value;
-                    // Display the search results in the main content
-                    let resultsContainer = document.getElementById('searchResults');
-                    resultsContainer.innerHTML = ("<h3>5 Day Forecast for </h3>" +
-                        searchTerm);
+                        let searchTerm = document.querySelector('#search-input').value;
+                        // Display the search results in the main content
+                        let resultsContainer = document.getElementById('searchResults');
+                        resultsContainer.innerHTML = ("<h3>5 Day Forecast for </h3>" +
+                            searchTerm);
 
-                    // // put icon in place
-                    iconDeploy();
-                    function iconDeploy() {
-                    let iconUrl = 'https://openweathermap.org/img/wn/' + weatherIconCode + '@2x.png';
-                                        
-                    let iconDisplay = document.createElement('img');
+                        // // put icon in place
+                        iconDeploy();
+                        function iconDeploy() {
+                            let iconUrl = 'https://openweathermap.org/img/wn/' + weatherIconCode + '@2x.png';
 
-                    resultsContainer.append(iconDisplay);
+                            let iconDisplay = document.createElement('img');
 
-                    iconDisplay.src=iconUrl;
+                            resultsContainer.append(iconDisplay);
+
+                            iconDisplay.src = iconUrl;
+                        }
+
+                        let weatherResults = document.createElement('div');
+
+
+                        resultsContainer.append(weatherResults);
+
+                        weatherResults.innerHTML = (' <h5>Today</h5>' + currentTime + ' Temp: ' + currentTemp + 'c ' + 'Humidity: ' + currentHumid + ' Wind Speed: ' + currentWind + 'kph');
+
+
+
+                        //for loop that gets every 8th index starting at 8 to get the next 5 days (there are 8 weather forecasts per day)
+
+                        for (i = 8; i <= 40; i += 8) {
+                            console.log(i);
+                            console.log(current.list[i]);
+                            let weatherIconCode = current.list[i].weather[0].icon;
+                            let currentTemp = current.list[i].main.temp;
+                            let currentHumid = current.list[i].main.humidity;
+                            let currentWind = current.list[i].wind.speed;
+                            let dt = current.list[i].dt;
+                            // console.log(dt);     
+                            // console.log(dayjs.unix(dt));       
+                            let unixFormat = dayjs.unix(dt).format('dddd');
+
+                            // Display the search results below the main content
+                            let resultsContainer = document.getElementById('fiveDayWeather');
+                            resultsContainer.classList.add("font-weight-bold", "col-12", "col-md-2", "col-lg-2", "m-1", "bg-light", "text-dark", 'border', 'border-dark', 'rounded');
+                            resultsContainer.innerHTML = (unixFormat);
+
+                            // // put icon in place
+                            function iconDeploy() {
+                                let iconUrl = 'https://openweathermap.org/img/wn/' + weatherIconCode + '@2x.png';
+
+                                let iconDisplay = document.createElement('img');
+
+                                resultsContainer.append(iconDisplay);
+
+                                iconDisplay.src = iconUrl;
+                            }
+
+                            let weatherResults = document.createElement('div');
+
+
+                            resultsContainer.append(weatherResults);
+
+                            weatherResults.innerHTML = (' Temp: ' + currentTemp + 'c ' + 'Humidity: ' + currentHumid + ' Wind Speed: ' + currentWind + 'kph');
+
+                            iconDeploy();
+                        }
                     }
-
-                    let weatherResults = document.createElement('div');
-
-                    
-                    resultsContainer.append(weatherResults);
-
-                    weatherResults.innerHTML = (' <h5>Today</h5>' + currentTime + ' Temp: ' + currentTemp + 'c ' + 'Humidity: ' + currentHumid + ' Wind Speed: ' + currentWind + 'kph');
-
-                }
-                //for loop that gets every 8th index starting at 8
-
-            })
+                })
 
 
-    });
+
+        });
 
 
 
 }
 
-    //save search term to local storage
-    function saveSearch() {
-        let searchInput = document.getElementById('search-input').value.trim();
-        if (!searchInput) return;
+//save search term to local storage
+function saveSearch() {
+    let searchInput = document.getElementById('search-input').value.trim();
+    if (!searchInput) return;
 
-        //get the existing search history
-        let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    //get the existing search history
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-        //add new search to array
-        searchHistory.unshift(searchInput);
+    //add new search to array
+    searchHistory.unshift(searchInput);
 
-        //limit search history to 5 items
-        searchHistory = searchHistory.slice(0, 5);
+    //limit search history to 5 items
+    searchHistory = searchHistory.slice(0, 5);
 
-        //save new search history
-        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+    //save new search history
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 
-        displaySearchHistory();
-        //do not have mulitple same entries
-    }
+    displaySearchHistory();
+    //do not have mulitple same entries
+}
 
-    function displaySearchHistory() {
-        let searchList = document.getElementById('city-history');
-        searchList.innerHTML = '';
-        // console.log(searchList);
-        let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+function displaySearchHistory() {
+    let searchList = document.getElementById('city-history');
+    searchList.innerHTML = '';
+    // console.log(searchList);
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-        //display search history on page
-        searchHistory.forEach(function (search) {
-            let listItem = document.createElement('li');
-            listItem.classList.add('p-2', 'bg-light', 'text-dark', 'border', 'border-dark', 'rounded')
-            listItem.textContent = search;
-            searchList.appendChild(listItem);
-            // add clickability to search history and trigger function getCityApi
-            listItem.addEventListener('click', function() {
-                searchTerm.value = search;
-                console.log(search);
-                getCityApi();
-            })
-        });
+    //display search history on page
+    searchHistory.forEach(function (search) {
+        let listItem = document.createElement('li');
+        listItem.classList.add('p-2', 'bg-light', 'text-dark', 'border', 'border-dark', 'rounded')
+        listItem.textContent = search;
+        searchList.appendChild(listItem);
+        // add clickability to search history and trigger function getCityApi
+        listItem.addEventListener('click', function () {
+            searchTerm.value = search;
+            console.log(search);
+            getCityApi();
+        })
+    });
 
-    };
-    // displaySearchHistory();
+};
+// displaySearchHistory();
 
-    // document.querySelector('#search-input').value;
+// document.querySelector('#search-input').value;
